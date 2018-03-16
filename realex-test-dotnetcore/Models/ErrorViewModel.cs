deea6c449ad1728cf77f389c9173aef1e41227ab
Oliver Wood <oliver.wood@bitetos.com>
2018-03-16 13:14:52 +0000

    Initial commit

---
+++ realex-test-dotnetcore/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace realex_test_dotnetcore.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
